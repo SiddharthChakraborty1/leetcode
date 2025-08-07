ae6b021c68a7a9d597ec89f689de25775853e6c3
Siddharth Chakraborty <sc9921777@gmail.com>
2025-08-07 22:04:43 +0530

    Removed function calls for evaluation

--- reverse_polish_notation.py
+++ reverse_polish_notation.py
@@ -2,38 +2,28 @@ class Solution:
     
     def __init__(self, *args, **kwargs):
         self.stack = []
-        self.operators = ["+", "-", "*", "/"]
-        self.operator_function_mapper = {
-            "+": self._add,
-            "-": self._subtract,
-            "*": self._mul,
-            "/": self._div
-        }
+        self.operators = ("+", "-", "*", "/")
         super().__init__(*args, **kwargs)
+    
+    def evaluate(self, operator_1, operator_2, operator):
+        if operator == "+":
+            return operator_1 + operator_2
+        elif operator == "-":
+            return operator_1 - operator_2
+        elif operator == "*":
+            return operator_1 * operator_2
+        elif operator == "/":
+            return int(operator_1/operator_2)
 
-    
-    def _add(self, operator_1, operator_2):
-        return operator_1 + operator_2
-    
-    def _subtract(self, operator_1, operator_2):
-        return operator_1 - operator_2
-    
-    def _mul(self, operator_1, operator_2):
-        return operator_1 * operator_2
-    
-    def _div(self, operator_1, operator_2):
-        return operator_1 / operator_2 # Truncates towards zero (floor but also with negative numbers). // will move -3.5 to 4 instead of 3
     
     def evalRPN(self, tokens):
         for token in tokens:
-            if token not in self.operator_function_mapper:
+            if token not in self.operators:
                 self.stack.append(token)
             else:
                 operator_2 = int(self.stack.pop())
                 operator_1 = int(self.stack.pop())
-                result = self.operator_function_mapper.get(token)(operator_1, operator_2)
+                result = self.evaluate(operator_1, operator_2, token)
                 self.stack.append(result)
 
         return int(self.stack[0])
-
-
